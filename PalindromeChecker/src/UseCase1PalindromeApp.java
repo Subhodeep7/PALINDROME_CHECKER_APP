f9c27b36073887e0e4d41353f37f6e3dd3046618
Subhodeep7 <mitrasubhodeep248@gmail.com>
2026-02-27 09:30:59 +0530

    UC1:Add welcome message module

---
+++ PalindromeChecker/src/UseCase1PalindromeApp.java
@@ -0,0 +1,14 @@
+
+
+public class UseCase1PalindromeApp {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(" Welcome to Palindrome Checker System ");
+        System.out.println(" Version: 1.0");
+
+        System.out.println(" System initialized successfully.");
+    }
+}
