08cf4cf15dfacafc8c03a91079789006fa23120a
Subhodeep Mitra <mitrasubhodeep248@gmail.com>
2026-03-07 20:41:52 +0530

    UC3: Palindrome check using string reverse

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -10,21 +12,37 @@ class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker App!");
         System.out.println();
 
-        // UC2: Hardcoded Palindrome Check
+        // UC2: Hardcoded Palindrome Check (two pointer method)
         String input = "madam";
         int n = input.length();
         boolean isPalindrome = true;
 
         for (int i = 0; i < n / 2; i++) {
-            // Compare characters from both ends
             if (input.charAt(i) != input.charAt(n - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
         }
 
+        System.out.println("UC2 Result:");
         System.out.println("Input text: " + input);
         System.out.println("Is it a palindrome? : " + isPalindrome);
+        System.out.println();
+
+        // UC3: Palindrome check using reverse string
+        String rev = "";
+
+        for (int i = n - 1; i >= 0; i--) {
+            rev += input.charAt(i);
+        }
+
+        System.out.println("UC3 Result:");
+
+        if (rev.equals(input)) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
 
     }
 }
