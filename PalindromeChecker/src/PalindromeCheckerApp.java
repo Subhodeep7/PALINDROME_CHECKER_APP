f8d1d8c360f9aac1b37e3eaf940a052c69924b01
Subhodeep Mitra <mitrasubhodeep248@gmail.com>
2026-03-07 20:40:51 +0530

    UC2: Hardcoded palindrome check

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,11 +1,20 @@
-import java.util.*;
-import java.lang.String;
 class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+
+        // UC1: Application Entry & Welcome Message
+        System.out.println("=================================");
+        System.out.println("        PALINDROME CHECKER       ");
+        System.out.println("=================================");
+        System.out.println("Application Version: 1.0");
+        System.out.println("Welcome to the Palindrome Checker App!");
+        System.out.println();
+
+        // UC2: Hardcoded Palindrome Check
         String input = "madam";
         int n = input.length();
         boolean isPalindrome = true;
+
         for (int i = 0; i < n / 2; i++) {
             // Compare characters from both ends
             if (input.charAt(i) != input.charAt(n - 1 - i)) {
@@ -14,7 +23,8 @@ class PalindromeCheckerApp {
             }
         }
 
-        System.out.println("Input text: "+input);
-        System.out.println("Is it a palindrome? : "+isPalindrome);
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a palindrome? : " + isPalindrome);
+
     }
 }
